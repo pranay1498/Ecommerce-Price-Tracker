0f025a25ae8cc13f91e33e801a2c8bc1d1f489e3
pranay1498 <53835253+pranay1498@users.noreply.github.com>
2021-02-01 01:57:13 +0530

    Update settings.py

--- FirstProject/FirstProject/settings.py
+++ FirstProject/FirstProject/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "productscrapers@gmail.com"
-EMAIL_HOST_PASSWORD = "scraper@pranay"
+EMAIL_HOST_USER = "EMAIL"
+EMAIL_HOST_PASSWORD = "PASSWORD"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
